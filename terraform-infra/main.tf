c7252f1f78f511e3a058b2923c585b6a6f3210a9
Anupam129 <97075245+Anupam129@users.noreply.github.com>
2025-11-29 10:56:27 +0530

    Update AWS provider configuration in main.tf

--- terraform-infra/main.tf
+++ terraform-infra/main.tf
@@ -1,14 +1,16 @@
-terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+provider "aws" {
+  region     = "ap-southeast-2"
+  access_key = "AKIAXEZ6JST5DgYN6DGZ"
+  secret_key = "ZhWe0GKwBQ54DGuuR6Czm5eaaDkjAr63jVb5abvq"
 }
 
+resource "aws_vpc" "devops_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "AmazonVPC"
+  }
+}
 
 
 
